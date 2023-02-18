19069a457ed8491aa2325e405bd14bf74ed3fb78
vanelk <ditrix.360@gmail.com>
2023-02-17 20:15:12 -0500

    gh-page configs

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 
 export default function NavBar(){
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary px-5">
-        <a class="navbar-brand" href="/">Questions</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary px-5">
+        <Link className="navbar-brand" href="/">Questions</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-            <li class="nav-item">
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+            <li className="nav-item">
                 <Link className="nav-link" to="/change-password">Change Password</Link>
             </li>
             <li class="nav-item">
